9b37e7f4f2b5d70b2aaf3d2b163a4a1ab3186577
Shivani <shivani332433@gmail.com>
2025-08-22 22:43:01 +0530

    Ice cream Shop Spring

--- ice-cream-shop/src/main/java/com/xworkz/ice_cream/config/IceCreamConfiguration.java
+++ ice-cream-shop/src/main/java/com/xworkz/ice_cream/config/IceCreamConfiguration.java
@@ -16,9 +16,6 @@ public class IceCreamConfiguration {
     public IceCreamConfiguration(){
         System.out.println("created Ice_creamConfiguration");
     }
-
-
-
     @Bean
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
