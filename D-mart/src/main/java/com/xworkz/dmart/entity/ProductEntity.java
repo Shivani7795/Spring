483f4c86521cd143391faf52032642314fc37852
Shivani <shivani332433@gmail.com>
2025-09-11 23:39:46 +0530

    JPA

--- D-mart/src/main/java/com/xworkz/dmart/entity/ProductEntity.java
+++ D-mart/src/main/java/com/xworkz/dmart/entity/ProductEntity.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
 import javax.persistence.*;
-
 @Getter
 @Setter
 @RequiredArgsConstructor
